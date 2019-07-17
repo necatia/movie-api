0af2fd31fabaf0ec2a09455566ce80176cf38ddd
necatia <necati.arslan2018@gmail.com>
2019-07-17 16:24:11 +0300

    director işlemleri

--- routes/director.js
+++ routes/director.js
@@ -20,4 +20,152 @@ router.post('/',(req,res,next)=>{
     
 });
 
+
+
+/*router.get('/',(req,res,next)=>{
+
+    const promise = Director.find({});
+
+    promise.then((data)=>{
+      res.json(data);
+    }).catch((err)=>{
+      res.json(err);
+    })  
+});*/
+
+
+router.get('/',(req,res,next)=>{
+
+    const promise = Director.aggregate([
+        {
+			$lookup: {
+				from: 'movies',
+				localField: '_id',
+				foreignField: 'director_id',
+				as: 'movies'
+			}
+		},
+		{
+			$unwind: {
+				path: '$movies',
+				preserveNullAndEmptyArrays: true
+			}
+		},
+		{
+			$group: {
+				_id: {
+					_id: '$_id',
+					name: '$name',
+					surname: '$surname',
+					bio: '$bio'
+				},
+				movies: {
+					$push: '$movies'
+				}
+			}
+		},
+		{
+			$project: {
+				_id: '$_id._id',
+				name: '$_id.name',
+                surname: '$_id.surname',
+                bio:'$bio',
+                movies: '$movies'
+               
+			}
+		}
+    ]);
+
+    promise.then((data)=>{
+      res.json(data);
+    }).catch((err)=>{
+      res.json(err);
+    })  
+});
+
+router.get('/:director_id',(req,res,next)=>{
+
+    const promise = Director.aggregate([
+        {
+            $match:{
+                '_id':mongoose.Types.ObjectId(req.params.director_id)
+            }
+        },       
+        {
+			$lookup: {
+				from: 'movies',
+				localField: '_id',
+				foreignField: 'director_id',
+				as: 'movies'
+			}
+		},
+		{
+			$unwind: {
+				path: '$movies',
+				preserveNullAndEmptyArrays: true
+			}
+		},
+		{
+			$group: {
+				_id: {
+					_id: '$_id',
+					name: '$name',
+					surname: '$surname',
+					bio: '$bio'
+				},
+				movies: {
+					$push: '$movies'
+				}
+			}
+		},
+		{
+			$project: {
+				_id: '$_id._id',
+				name: '$_id.name',
+                surname: '$_id.surname',
+                bio:'$bio',
+                movies: '$movies'
+               
+			}
+		}
+    ]);
+
+    promise.then((data)=>{
+      res.json(data);
+    }).catch((err)=>{
+      res.json(err);
+    })  
+});
+
+router.put('/:director_id', (req, res, next) => {
+	const promise = Director.findByIdAndUpdate(
+		req.params.director_id,
+		req.body,
+		{
+			new: true
+		}
+	);
+
+	promise.then((director) => {
+		if (!director)
+			next({ message: 'The director was not found.', code: 99 });
+
+		res.json(director);
+	}).catch((err) => {
+		res.json(err);
+	});
+});
+
+router.delete('/:director_id', (req, res, next) => {
+	const promise = Director.findByIdAndRemove(req.params.director_id);
+
+	promise.then((director) => {
+		if (!director)
+			next({ message: 'The director was not found.', code: 99 });
+
+		res.json({ status: 1 });
+	}).catch((err) => {
+		res.json(err);
+	});
+});
 module.exports = router;
